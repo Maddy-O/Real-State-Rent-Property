71fa815d73ba5f0d9d0be7e8f24c8405feb1548a
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-11-10 16:55:46 +0530

    Spreading data in Property component
    1. spreading received data from parent
    2. using inline style for initial

--- src/Components/PropertyCompo.jsx
+++ src/Components/PropertyCompo.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
-const PropertyCompo = () => {
-  return <div>PropertyCompo</div>;
+const PropertyCompo = ({ property }) => {
+  console.log(property);
+  return (
+    <div style={{ width: "250px", textAlign: "left" }}>
+      <div
+        style={{
+          backgroundColor: `${property.img}`,
+          width: "250px",
+          height: "150px",
+        }}
+      ></div>
+      <div style={{ padding: "0px 7px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p style={{ marginBottom: "5px" }}>
+            <b style={{ fontSize: "20px", color: "#7165eb" }}>
+              {property.price}
+            </b>
+            /month
+          </p>
+          <button
+            style={{
+              width: "35px",
+              height: "35px",
+              border: "1px solid gray",
+              borderRadius: "50%",
+            }}
+          >
+            ♡
+          </button>
+        </div>
+        <h3 style={{ margin: "0px" }}>{property.name}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default PropertyCompo;
