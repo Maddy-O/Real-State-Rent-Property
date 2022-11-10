ad876ec10708600c03cd47bdb036e9f72e755fa8
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-11-10 16:56:55 +0530

    added navbar to route
    1. added navbar in route so we can access it in webpage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import FavouritesPage from "./Pages/FavouritesPage";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
